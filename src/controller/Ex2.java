d21a12f2869263693a499d3a3ffb7ab473abc244
Rafael Sartori <160548884+RafaelLSartori@users.noreply.github.com>
2024-08-16 12:17:53 -0300

    Atualização

--- src/controller/Ex2.java
+++ src/controller/Ex2.java
@@ -1,2 +1,19 @@
-package controller;public class Ex2 {
+package controller;
+
+public class Ex2 {
+    public Ex2(){
+        super();
+    }
+    public int freccontador(int n1, int n2, int cta){
+        //A condição de para foi pensada no resultado da divisão, quando chegar a 0, retorna o contador.
+        if (n1 == 0){
+            return cta;
+        }
+        //Se o primeiro digitado divido por 10 for igual ao segundo número, o contador aumenta.
+        if (n1 % 10 == n2){
+            cta++;
+        }
+        //A chamada da recursiva divide o número por 10 até que ele entre no ponto de parada.
+        return freccontador(n1 / 10, n2, cta);
+    }
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,22 @@
+package view;
+
+import controller.Ex2;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ex2 m = new Ex2();
+        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor de 10 a 999999: "));
+        while (num1 < 10 || num1 > 999999){
+            JOptionPane.showMessageDialog(null,"O número precisa ser de 10 a 999999!");
+            num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite novamente um valor: "));
+        }
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor de 0 a 9: "));
+        while (num2 < 0 || num2 > 9){
+            JOptionPane.showMessageDialog(null,"O número precisa ser de 0 a 9!");
+            num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite novamente um valor: "));
+        }
+        System.out.println(m.freccontador(num1, num2, 0));
     }
 }
